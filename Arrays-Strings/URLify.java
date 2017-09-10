47fba2fb0b926c7361a4eafd9eb816673ba9840f
Soniya Sadalkar <soniyasadalkar@Soniyas-MacBook-Pro.local>
2017-09-09 17:40:07 -0700

    Replace the true spaces in a string with '%20'. Given that the input string has enough space at the end to hold the extra characters.

---
+++ Arrays-Strings/URLify.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class URLify{
+	public static void main(String args[]){
+		String input = "Mr John Smith Gold      ";
+		int truelength = 18;
+		char[] urlified_str = getUrlifiedStr(input.toCharArray(), truelength);
+		System.out.println("Orginal String: "+input);
+		System.out.println("URLified String: "+ Arrays.toString(urlified_str));
+	}
+
+	public static char[] getUrlifiedStr(char[] input, int truelength){
+		/* first count the number of true spaces */
+		int spaceCount = 0;
+		int index = 0;
+		for( int i = 0; i < truelength ; i++)
+		{
+			if(input[i] == ' ')
+				++spaceCount;
+		}
+		index = truelength + spaceCount * 2;
+		if( truelength < input.length )
+			input[truelength] = '\0';
+
+		for( int i = truelength - 1; i >= 0 ; --i)
+		{
+			if( input[i] == ' ' )
+			{
+				input[index - 1] = '0';
+				input[index - 2] = '2';
+				input[index - 3] = '%';
+				index = index - 3;
+			}
+			else{
+				input[index - 1] = input[i];
+				index--;
+			}
+		}
+		return input;
+	}
+}
