03a91896dd1c6e41160b1a545d383ec57f0b73a6
Soniya Sadalkar <soniyasadalkar@Soniyas-MacBook-Pro.local>
2017-09-10 18:11:20 -0700

    implemented the toggle version for checking the palindrome.

--- Arrays-Strings/CheckPalindrome.java
+++ Arrays-Strings/CheckPalindrome.java
@@ -8,6 +8,10 @@ class CheckPalindrome{
 		else
 			System.out.println("No! "+input+" does NOT have a permutation which is palindrome");
 
+		if( checkPalindrome_v2(input) )
+			System.out.println("Yes! "+input+" has a permutation which is palindrome");
+		else
+			System.out.println("No! "+input+" does NOT have a permutation which is palindrome");
 	}
 
 	public static boolean checkChar(char ch){
@@ -36,5 +40,43 @@ class CheckPalindrome{
 			}
 		}
 		return (chars_list.size() <= 1);
+	}
+
+	public static boolean checkPalindrome_v2(String input){
+		int bitVector = createBitVector(input);
+		return (bitVector == 0 || checkExactlyOneBitSet(bitVector));
+	}
+
+	public static boolean checkExactlyOneBitSet(int bitVector){
+		return (bitVector & (bitVector - 1)) == 0;
+	}
+
+	public static int getChar(char ch){
+		int a = Character.getNumericValue('a');
+		int z = Character.getNumericValue('z');
+		int val = Character.getNumericValue(ch);
+		if( a <= val && z >= val)
+			return val - a;
+		return -1;
+	}
+	public static int createBitVector(String input){
+		int bitVector = 0;
+		for( char ch : input.toCharArray() ){
+			int index = getChar(ch);
+			bitVector = toggleVector(bitVector, index);
+		}
+		return bitVector;
+	}
+	
+	public static int toggleVector(int bitVector, int index){
+		if( index < 0) 
+			return bitVector;
+		int mask = (1 << index);
+		if( (bitVector & mask) == 0)
+			bitVector |= mask;
+		else
+			bitVector &= ~mask;
+		
+		return bitVector;
 	}
 }
